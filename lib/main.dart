b2fb34df8856b59438ca6de47d08714160302e6e
Kishikawa1286 <k.ishikawa.gude@gmail.com>
2022-02-25 18:44:31 +0900

    dart-define設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,35 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
 import 'utils/on_generate_route.dart';
 
-void main() {
-  runApp(
-    const ProviderScope(
-      child: MaterialApp(
-        title: 'n-sneaker',
-        onGenerateRoute: onGenerateRoute,
-        initialRoute: '/',
-      ),
-    ),
+Future<void> main() async {
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      await PackageInfo.fromPlatform().then((value) {
+        print(value.appName);
+        print(value.packageName);
+      });
+      runApp(
+        const ProviderScope(
+          child: MaterialApp(
+            title: 'n-sneaker',
+            onGenerateRoute: onGenerateRoute,
+            initialRoute: '/',
+          ),
+        ),
+      );
+    },
+    (error, stackTrace) async {
+      print('runZonedGuarded: Caught error in my root zone.');
+      print('--- error ---\n$error\n-------------');
+      print('--- stackTrace ---\n$stackTrace\n------------------');
+    },
   );
 }
