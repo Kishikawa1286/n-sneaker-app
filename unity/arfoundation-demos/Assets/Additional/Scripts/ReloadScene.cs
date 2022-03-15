5405bdd9b5aa85fd4f2b8c722ca861a0b670f53b
Kentaro Ishikawa <kentaroishikawa1286@gmail.com>
2022-03-15 10:05:49 +0900

    リロードを試みる

--- unity/arfoundation-demos/Assets/Additional/Scripts/ReloadScene.cs
+++ unity/arfoundation-demos/Assets/Additional/Scripts/ReloadScene.cs
@@ -2,13 +2,20 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
+// using UnityEngine.XR.ARSubsystems;
 
 public class ReloadScene : MonoBehaviour, IEventSystemHandler
 {
+    [SerializeField]
+    private ARSession session;
+
     // This method is called from Flutter
     public void Reload(String message)
     {
-        // https://unity-yuji.xyz/active-scene-re-load/
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GameObject target = GameObject.Find("Target Sneaker");
+        Destroy(target);
+
+        session.Reset();
     }
 }
