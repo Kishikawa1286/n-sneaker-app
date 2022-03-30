fa9977b20b3ad83e9e62a880dfa20391e0b5bf45
izumi0x01 <izumisoh.develop@gmail.com>
2022-03-30 11:58:00 +0900

    エフェクトウィンドウとの切り替え完了

--- unity/arfoundation-demos/Assets/Additional/Scripts/Presenter/ObjectDisplayPresenter.cs
+++ unity/arfoundation-demos/Assets/Additional/Scripts/Presenter/ObjectDisplayPresenter.cs
@@ -8,7 +8,6 @@ public class ObjectDisplayPresenter : MonoBehaviour
 {
   // Start is called before the first frame update
     [SerializeField] Button _ScreenShotButton;
-    [SerializeField] Toggle _ChangeEffectToggle;
     [SerializeField] Button _ReloadButton;
     [SerializeField] ScreenshotModel _ScreenShotModel;
     [SerializeField] SceneReloadModel _SceneReloadModel;
@@ -21,20 +20,24 @@ public class ObjectDisplayPresenter : MonoBehaviour
     void Start()
     {
         //from View to Model
+        //subscribeは，Subjectに実行してほしい関数を登録する処理
         _ReloadButton.onClick.AsObservable().Subscribe(_ => _SceneReloadModel.SceneReload());
         _ScreenShotButton.onClick.AsObservable().Subscribe(_ => _ScreenShotModel.Screenshot());
 
-        //UniRXの仕様がよくわかってないので，一度保留．
+    //UniRXの仕様がよくわかってないので，一度保留．
+    //ViewのToggleの値が変更された場合，Modelへ通知する．さらに，bool値の状態を変更する．
+    //（.OneNextは強制的にイベントを発効するだけ．別にイベントが発効できるなら，.OnNextを使わなくてもよい）
+    _SwitchEffectWindowModel.IsOn.Subscribe(_SwitchEffectWindowView.SwitchEffectWindow);
+    _SwitchEffectWindowView.OnToggleClicked.Subscribe(_SwitchEffectWindowModel.SwitchValue);
 
-        //ViewのToggleの値が変更された場合，Modelへ通知する．さらに，bool値の状態を変更する．
-        //_SwitchEffectWindowView.OnToggleClicked.Subscribe(_SwitchEffectWindowModel.SwitchValue).AddTo(this);
-        // _ChangeEffectToggle.OnValueChangedAsObservable()
-        // .Where(x => x == true)
-        // .Subscribe(_ChangeEffectToggle.isOn => 
-        // {
-        //     _ScreenShotButton.GetComponent<GameObject>().SetActive(false);
-        //     _ScreenShotButton.GetComponent<GameObject>().SetActive(false);
-        // });
+    //_SwitchEffectWindowView.OnToggleClicked.Subscribe(_SwitchEffectWindowModel.SwitchValue).AddTo(this);
+    // _ChangeEffectToggle.OnValueChangedAsObservable()
+    // .Where(x => x == true)
+    // .Subscribe(_ChangeEffectToggle.isOn => 
+    // {
+    //     _ScreenShotButton.GetComponent<GameObject>().SetActive(false);
+    //     _ScreenShotButton.GetComponent<GameObject>().SetActive(false);
+    // });
 
   }
 }
